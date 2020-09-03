b0aecb3c10cb23a84faf2505d3de816126a88e5e
Zane Kleinberg <zaneskleinberg@gmail.com>
2020-09-03 14:54:34 -0400

    Files->Github

--- CBPatcher.c
+++ CBPatcher.c
@@ -13,11 +13,21 @@
 #include "CBPUtils.h"
 #include "CBPatch.h"
 
+int patch_kernel(char* infile, char* outfile, char* version);
+
 int main(int argc, char **argv) {
     
-    if (argc < 4) {
-        printf("Missing args (cbpatcher infile outfile version)\n");
+    if (argc < 4 || 5 < argc) {
+        printf("Missing args (cbpatcher infile outfile version [--nosb])\n");
         return -1;
+    }
+    
+    int nukesb = 1;
+    
+    if (argc == 5) {
+        if(!strcmp(argv[4], "--nosb")){
+            nukesb = 0;
+        }
     }
     
     char *fileName = argv[1];
@@ -35,7 +45,7 @@ int main(int argc, char **argv) {
     
     char *versionNum = argv[3];
     
-    ret = kernPat(fileBuf, fileLen, versionNum);
+    ret = kernPat(fileBuf, fileLen, versionNum, nukesb);
     
     if (ret) {
         printf("Failed to patch kernel\n");
@@ -46,6 +56,45 @@ int main(int argc, char **argv) {
     printf("Kernel patched successfully\n");
     
     FILE *outFile = fopen(argv[2], "w");
+    fwrite(fileBuf, fileLen, 1, outFile);
+    fflush(outFile);
+    fclose(outFile);
+    
+    free(fileBuf);
+    
+    return 0;
+}
+
+int patch_kernel(char* infile, char* outfile, char* version) {
+    
+    int nukesb = 1;
+    
+    char *fileName = infile;
+    
+    void *fileBuf = 0;
+    
+    size_t fileLen = 0;
+    
+    int ret = openFile(fileName, &fileLen, &fileBuf);
+    
+    if (ret) {
+        printf("Failed to open %s\n", fileName);
+        return -1;
+    }
+    
+    char *versionNum = version;
+    
+    ret = kernPat(fileBuf, fileLen, versionNum, nukesb);
+    
+    if (ret) {
+        printf("Failed to patch kernel\n");
+        free(fileBuf);
+        return -1;
+    }
+    
+    printf("Kernel patched successfully\n");
+    
+    FILE *outFile = fopen(outfile, "w");
     fwrite(fileBuf, fileLen, 1, outFile);
     fflush(outFile);
     fclose(outFile);
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # CBPatcher
 A tool to patch a 32 bit iOS kernelcache for use in a jailbroken environment.
 
-iOS 9.x and 10.x support initially, more OS's to come.
+***iOS 4.x to 10.x is supported.***
+
+## Usage
+
+    ./CBPatcher <in> <out> <version>
+    
+
+## Notes
+     * Please report any versions you find with issues! This is an ongoing project...
